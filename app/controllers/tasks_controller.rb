b498e28835791300ed4a4483fe95e130f0bf068a
Arnaud Turnani <arnaud.tur@live.fr>
2020-08-11 14:55:28 +0200

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,11 +9,35 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new # needed to instantiate the form_for
+
   end
+
+
 
   def create
     @task = Task.new(task_params)
+    redirect_to index_path
     @task.save
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    # no need for app/views/tasks/update.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/tasks/destroy.html.erb
+    redirect_to tasks_path
   end
 
   private
